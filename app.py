b3b167bbba788873dbeddcc9ae2c9f3149c2e343
GANAPATHI S <vengatachalag@gmail.com>
2025-05-07 21:48:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import io
 
 # Function to load the color dataset
 @st.cache_data
-def load_color_dataset(file_path):
+def load_color_dataset(colors.csv):
     try:
         df = pd.read_csv(file_path)
         if not all(col in df.columns for col in ['name', 'red', 'green', 'blue']):
